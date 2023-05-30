3fbf8b0da9c1a7f747555ba18a173d3b1e2d2ba0
shoaib-vaival <muhammad.shoaib@vaival.com>
2023-05-30 15:58:15 +0500

    listed the nft in my collection
    Listed the nft on my collection page from api and created loader component

--- pages/my-collection.tsx
+++ pages/my-collection.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
 import { Header } from '../src/components/Header'
-import CollectionCards from '../src/components/Cards/CollectionCard'
+import CollectionCard from '../src/components/Cards/CollectionCard'
 import { Box, Container, Flex, Heading, Text, Button, Square } from '@chakra-ui/react'
+import { useQuery } from '../src/hooks/useQuery'
+import { Loader } from '../src/components/Loader'
 
 
 const myCollection: NextPage = () => {
+
+  const {isLoading, data} = useQuery<any>({
+    queryKey: ['get-collection'],
+    url: 'collection/getCollectionsByWalletAddress/0x0000',
+    showToast: false,
+  });
 
   return (
     <div>
@@ -32,24 +40,11 @@ const myCollection: NextPage = () => {
         <Container maxW={{md: '4xl', xl: '8xl' }} pb={{ xl: '100px', md: '50px' }}>
           <Box>
             <Flex direction={['column', 'row']} flexWrap='wrap'>
-              <Square w={{ xl: '25%', md: '50%',sm:'50%' }} display='initial'>
-                <CollectionCards />
-              </Square>
-              <Square w={{ xl: '25%', md: '50%',sm:'50%' }} display='initial'>
-                <CollectionCards />
-              </Square>
-              <Square w={{ xl: '25%', md: '50%',sm:'50%' }} display='initial'>
-                <CollectionCards />
-              </Square>
-              <Square w={{ xl: '25%', md: '50%',sm:'50%' }} display='initial'>
-                <CollectionCards />
-              </Square>
-              <Square w={{ xl: '25%', md: '50%',sm:'50%' }} display='initial'>
-                <CollectionCards />
-              </Square>
-              <Square w={{ xl: '25%', md: '50%',sm:'50%' }} display='initial'>
-                <CollectionCards />
-              </Square>
+              {isLoading && data === undefined?<Flex width="100%" height="100%" justifyContent='center' alignItems="center"><Loader/></Flex>:
+              data.map((nftCollection, index)=>{
+                  return <CollectionCard logoImage = {nftCollection.logoImageUrl} featureImage = {nftCollection.bannerImageUrl} name= {nftCollection.name} volume='-.-' price='-.-'  />
+                })
+              }
             </Flex>
 
           </Box>
