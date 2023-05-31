ad0b1aa2f2f623f620b1a0e28841a76a052d70f1
M-Arsalan123 <arsalan.munir@vaival.com>
2023-05-31 12:18:08 +0500

    integrated collection prices with field validations and dynamic form fields, put some types

--- pages/create-collection/schema.ts
+++ pages/create-collection/schema.ts
@@ -5,6 +5,17 @@ export const collectionSchema = Yup.object().shape({
     .nullable()
     .max(50, 'Collection name must not exceed 50 characters'),
     description: Yup.string().nullable().max(1000, 'Collection detail must not exceed 1000 characters'),
+    category: Yup.string()
+    .required('Categorye is required'),
     logoImageUrl: Yup.string()
-      .required('Logo image is required')
+      .required('Logo image is required'),
+      creatorFee: Yup.array().of(
+        Yup.object().shape({
+          walletAddress: Yup.string().required('Wallet address is required'),
+          percentage: Yup
+          .number()
+          .max(10, 'Percentage should be less than 10')
+          .required('Value is required'),
+        })
+      ),
 });
--- src/components/common/ReactSelect/types.ts
+++ src/components/common/ReactSelect/types.ts
@@ -2,3 +2,15 @@ export type ReactSelectTypes = {
  label: string;
  value: string;
 }
+
+export type ReactSelectPropsTypes = {
+    options: any;
+    isMultiple: boolean;
+    getSelectedData: any;
+    identifier: string;
+    label?: string;
+   }
+
+   export type ReactSelectCatMap = {
+        map(arg0: (category: any) => any): unknown;label: string; value: string; length: number
+   }
