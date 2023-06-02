89992da22e9217c567d8f63776e3025ecf05f6d4
shoaib-vaival <muhammad.shoaib@vaival.com>
2023-06-02 18:37:34 +0500

    Home page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {Container,Stat, StatLabel, StatHelpText,StatNumber, Card, CardBody,Image
 import { SlickSlider } from '../src/components/ReactSlick'
 import CollectionCard from '../src/components/Cards/CollectionCard'
 import { Banner } from '../src/components/Banner'
+import {FilterTabs} from '../src/components/FilterTabs'
 
 
 
@@ -12,73 +13,130 @@ const Home: NextPage = () => {
   return (
     <div>
       <Header />
+      <Box bgImage = '/assets/images/bg-lines.png'>
       <Banner/>
-      <Container maxW="">
+      </Box>
+      <Container mt='80px'>
+        <Heading>Featured Collections</Heading>
         <SlickSlider >
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
-          <CollectionCard type="withoutBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withoutBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
           </SlickSlider>
       </Container>
-      <Card maxW='sm' marginLeft="20px" justifyContent="center" >
-  <CardBody display="flex" flexDirection="column" justifyContent="center">
+      <Container mt='80px'>
+        <Heading>Featured Collections</Heading>
+        <SlickSlider >
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          </SlickSlider>
+      </Container>
+      <Container  mt='80px' >
+        <Flex justifyContent = 'space-between' alignItems='center' mb='40px'>
+          <Heading>Browse by Categories</Heading>
+          <Button variant='primary'>View All</Button>
+        </Flex>
+      <FilterTabs tabsList = {['All', 'Art', 'Gaming', 'Membership', 'PFPs']} getTabIndex={(index)=>console.log(index)}/>
+      </Container>
+      <Container  mt='80px' >
+        <Flex justifyContent = 'space-between' alignItems='center' mb='40px'>
+          <Heading>Trending Collections</Heading>
+          <Button variant='primary'>View All</Button>
+        </Flex>
+       <SlickSlider >
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          </SlickSlider>
+      </Container>
+      <Container  mt='80px' >
+        <Flex justifyContent = 'space-between' alignItems='center' mb='40px'>
+          <Heading>Trending in Art</Heading>
+          <Button variant='primary'>View All</Button>
+        </Flex>
+       <SlickSlider >
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          </SlickSlider>
+      </Container>
+      <Container  mt='80px' >
+        <Flex justifyContent = 'space-between' alignItems='center' mb='40px'>
+          <Heading>Recent Collections</Heading>
+          <Button variant='primary'>View All</Button>
+        </Flex>
+       <SlickSlider >
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          <CollectionCard type='withBody' featureImage='/assets/images/nft1.png' isShowFeatureImage = {true} isShowLogoImage={false} name='Peppy Road'/>
+          </SlickSlider>
+      </Container>
+      {/* <Card maxW='sm' marginLeft='20px' justifyContent='center' >
+  <CardBody display='flex' flexDirection='column' justifyContent='center'>
     <Image
       src='/assets/images/nft1.png'
       alt='Green double couch with wooden legs'
       borderRadius='lg'
     />
-    <Stack mt='16px' spacing='3' px="24px">
-      <Heading size='20px' fontWeight="700" color="#0D0D0D">Living room Sofa</Heading>
+    <Stack mt='16px' spacing='3' px='24px'>
+      <Heading size='20px' fontWeight='700' color='#0D0D0D'>Living room Sofa</Heading>
       <SimpleGrid columns={[2, null, 2]} spacing='40px'>
         <Box >
-          <Text fontSize="14px" color="#756C99">Volume</Text>
-          <Text fontSize="20px" fontWeight="500" color="#393F59">1,118 ETH</Text>
+          <Text fontSize='14px' color='#756C99'>Volume</Text>
+          <Text fontSize='20px' fontWeight='500' color='#393F59'>1,118 ETH</Text>
         </Box>
         <Box  >
-          <Text fontSize="14px" color="#756C99">Floor Price</Text>
-          <Text fontSize="20px" fontWeight="500" color="#393F59">0.003 ETH</Text>
+          <Text fontSize='14px' color='#756C99'>Floor Price</Text>
+          <Text fontSize='20px' fontWeight='500' color='#393F59'>0.003 ETH</Text>
         </Box>
       </SimpleGrid>
     </Stack>
   </CardBody>
   <CardFooter>
-      <Button variant='primary' colorScheme='blue' w="100%">
+      <Button variant='primary' colorScheme='blue' w='100%'>
         Buy now
       </Button>
   </CardFooter>
 </Card>
-<Stat mb="200px">
+<Stat mb='200px'>
   <StatLabel>Collected Fees</StatLabel>
   <StatNumber>£0.00</StatNumber>
   <StatHelpText>Feb 12 - Feb 28</StatHelpText>
-</Stat>
-<Tabs>
-  <TabList>
-    <Tab>One</Tab>
-    <Tab>Two</Tab>
-    <Tab>Three</Tab>
-  </TabList>
+</Stat> */}
 
-  <TabPanels>
-    <TabPanel>
-      <p>one!</p>
-    </TabPanel>
-    <TabPanel>
-      <p>two!</p>
-    </TabPanel>
-    <TabPanel>
-      <p>three!</p>
-    </TabPanel>
-  </TabPanels>
-</Tabs>
 
-    </div>
+ </div>
   )
 }
 
--- src/theme/components/container.ts
+++ src/theme/components/container.ts
@@ -4,7 +4,9 @@ import { ComponentStyleConfig } from '@chakra-ui/theme';
 
 
 export const Container = {
-
+    baseStyle:{
+        maxW:{ sm: '4xl', lg: '6xl', xl: '8xl' }
+    },
     variants:{
         colorful:{
             bg: 'linear-gradient(98.11deg, #6F6BF3 12.05%, #A9A7F2 122.55%)',
