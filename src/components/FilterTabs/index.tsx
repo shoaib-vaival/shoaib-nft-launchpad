7301cb5ebb43aaec8983ea05c9506dcf57452ba2
awais-v <102358565+awais-v@users.noreply.github.com>
2023-06-06 17:34:21 +0500

    Silder columns changes

--- src/components/FilterTabs/index.tsx
+++ src/components/FilterTabs/index.tsx
@@ -13,7 +13,7 @@ export const FilterTabs = ({tabsList, getTabIndex}:FilterTabs) => {
             <TabList pl='0' flexWrap='wrap' borderBottom={{base:'0px solid rgba(53, 53, 53, 0.2)',sm:'1px solid rgba(53, 53, 53, 0.2)'}}>
         {tabsList && tabsList?.map((value, index)=> <Tab key = {index}>{value}</Tab>)}
          </TabList>
-         <SimpleGrid columns={[1, 2, 4]} mt={6} spacing={3}>
+         <SimpleGrid columns={[1, 2, 2, 4]} spacingX='40px' spacingY='20px'>
         <CollectionCard  type="withBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
         <CollectionCard  type="withBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
         <CollectionCard  type="withBody" featureImage="/assets/images/nft1.png" isShowFeatureImage = {true} isShowLogoImage={false} name="Peppy Road"/>
