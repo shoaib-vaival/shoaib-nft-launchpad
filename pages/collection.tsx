59dbc714ce2f34d3ecc41c1f236a183aa2daeb81
awais-v <102358565+awais-v@users.noreply.github.com>
2023-06-09 14:45:13 +0500

    Create activity page

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -43,7 +43,9 @@ const Collection: NextPage = () => {
     <>
       <Container maxW={{ sm: 'xl', md: '3xl', lg: '5xl', xl: '7xl' }}>
         <Box px={{base:'0',md:'17px'}}>
-        <ProfileHeader socialIcons={socialIcons} />
+        <ProfileHeader 
+        socialIcons={socialIcons}
+        showSocialIcons={true}  />
         </Box>
         <ProfileDetail showStats = {true}/>
       </Container>
