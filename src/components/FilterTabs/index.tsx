acb8c8870e4dbe881d8845c9bd6fea91bac76a13
awais-v <awais.abid@vaivaltech.com>
2023-07-14 15:56:36 +0500

    Done

--- src/components/FilterTabs/index.tsx
+++ src/components/FilterTabs/index.tsx
@@ -36,9 +36,9 @@ export const FilterTabs = ({tabsList, getTabIndex}:FilterTabs) => {
         <>
         <Tabs>
         <Menu>
-            <TabList pl='0' overflowX={{base:'auto',md:'initial'}} mx='17px' borderBottom={{base:'0px solid rgba(53, 53, 53, 0.2)',sm:'1px solid rgba(53, 53, 53, 0.2)'}} gap={{base:'15px',md:'22px'}}>
-        {tabsList && tabsList?.slice(0,5).map((category, index)=> <Tab p='17px 8px 8px'  mb='0' _selected={{mb:'0', borderBottom:'2px solid #6863F3',color:'#6863F3',p:{base:'17px 8px'}}} onClick={()=>filterUsingTabs(category?.id)} key = {index}>{category?.name}</Tab>)}
-        {tabsList && tabsList?.length >= 5?<Tab _active={{bg:'transparent',color:'#6863F3'}}  _hover={{bg:'transparent',color:'#6863F3'}} as={MenuButton} p='17px 8px 8px'>More <span style={spanStyle}> <i className="icon-ChevronDown"></i></span></Tab>:''}
+            <TabList pl='0' overflowX={{base:'auto',md:'initial'}} overflowY={{base:'hidden',md:'initial'}} mx='17px' borderBottom={{base:'0px solid rgba(53, 53, 53, 0.2)',sm:'1px solid rgba(53, 53, 53, 0.2)'}} gap={{base:'15px',md:'22px'}}>
+        {tabsList && tabsList?.slice(0,5).map((category, index)=> <Tab p='17px 8px 8px'  mb='0' _selected={{mb:'0', borderBottom:'2px solid #6863F3',color:'#6863F3'}} onClick={()=>filterUsingTabs(category?.id)} key = {index}>{category?.name}</Tab>)}
+        {tabsList && tabsList?.length >= 5?<Tab _active={{bg:'transparent',color:'#6863F3'}}  _hover={{bg:'transparent',color:'#6863F3'}} whiteSpace='nowrap'as={MenuButton} p={{base:'15px 8px 8px', md:'17px 8px 8px'}}>More <span style={spanStyle}> <i className="icon-ChevronDown"></i></span></Tab>:''}
          </TabList>
          <MenuList w='150px' minW='150px' >
         {tabsList && tabsList?.slice(5,tabsList.length).map((category, index)=> {return(
