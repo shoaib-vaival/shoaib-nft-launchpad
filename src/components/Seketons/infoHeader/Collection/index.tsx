ad38807e59134d90d6957e37ba54ca17e41be4ec
awais-v <awais.abid@vaivaltech.com>
2023-07-17 18:51:05 +0500

    done

--- src/components/Seketons/infoHeader/Collection/index.tsx
+++ src/components/Seketons/infoHeader/Collection/index.tsx
@@ -8,6 +8,7 @@ const SkeletonLoader = () => {
             backgroundColor="gray.200"
             h={{ base: "220px", md: "400px" }}
             pl={{ base: "24px", md: "54px" }}
+            borderRadius='16px'
         >
             <Box position="absolute" transform="translateY(-50%)" bottom="-35%" backgroundColor="gray.100" height={{ base: "100px", md: "200px" }} width={{ base: "100px", md: "200px" }} borderRadius="16px" />
         </Container>
